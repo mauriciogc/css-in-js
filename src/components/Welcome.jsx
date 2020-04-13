b3e52d77e09cc2fbc1b299bc93e058138fb0ab88
mauriciogc <mauriciogc@gmail.com>
2020-04-13 14:17:26 -0500

    example: dinamic props

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,11 +4,16 @@ import styled from "styled-components";
 const Title = styled.h1`
 	font-size: 30px;
 	text-align: center;
-	color: #e95678;
+	color: ${props => (props.primary ? "#e95678" : "#25b0bc")}; //(C)
 `;
 
-const Welcome = () => {
-	return <Title>Hello World!</Title>;
+const Welcome = ({ name = "Stranger" }) => {
+	const isStranger = name === "Stranger"; //(A)
+	return (
+		<Title primary={isStranger}>
+			{/* (B) */}Hello {name}
+		</Title>
+	);
 };
 
 export default Welcome;
