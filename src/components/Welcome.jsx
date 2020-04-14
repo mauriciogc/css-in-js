3b4bcee522ad931a847a3c0fb6fd219172b42612
mauriciogc <mauriciogc@gmail.com>
2020-04-14 10:30:23 -0500

    Hello world

---
+++ src/components/Welcome.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Title = styled.h1`
+	font-size: 30px;
+	text-align: center;
+	color: #e95678;
+`;
+
+const Welcome = () => {
+	return <Title>Hello World!</Title>;
+};
+
+export default Welcome;
