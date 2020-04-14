1e27d7bcdf6c43d7624aa8aef3bb14bc5974ae6c
mauriciogc <mauriciogc@gmail.com>
2020-04-14 10:32:12 -0500

    Hello world! with object

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const Title = styled.h1`
+/*const Title = styled.h1`
 	font-size: 30px;
 	text-align: center;
 	color: #e95678;
-`;
+`;*/
+
+const Title = styled.h1({
+	fontSize: "30px",
+	textAlign: "center",
+	color: "#e95678"
+});
 
 const Welcome = () => {
 	return <Title>Hello World!</Title>;
