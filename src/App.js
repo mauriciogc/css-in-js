ab90f7901838a4fa53109ba86213c38140759d7c
mauriciogc <mauriciogc@gmail.com>
2020-04-14 10:54:32 -0500

    extend styles

--- src/App.js
+++ src/App.js
@@ -4,6 +4,5 @@ import Welcome from './components/Welcome'
 const App = () => (
   <div className="App">
     <Welcome />
-    <Welcome name="Mauricio"/>
   </div>);
 export default App;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { Fragment } from "react";
 import styled from "@emotion/styled";
 
-const Title = styled.h1(props => ({
-	fontSize: "30px",
-	textAlign: "center",
-	color: props.primary ? "#e95678" : "#25b0bc"
-}));
+const Button = styled.button({
+	fontSize: "14px",
+	fontWeight: "400",
+	backgroundColor: "#4a430b",
+	border: "none",
+	cursor: "pointer",
+	color: "#ffffff",
+	padding: "10px 20px",
+	borderRadius: "4px"
+});
 
-const Welcome = ({ name = "Stranger" }) => {
-	const isStranger = name === "Stranger";
-	return <Title primary={isStranger}>Hello {name}</Title>;
+const Link = styled(Button)({
+	color: "#4a430b",
+	backgroundColor: "transparent"
+});
+
+const Welcome = () => {
+	return (
+		<Fragment>
+			<Button>Click me!!</Button>
+			<Link as="a">Give me click too!!</Link>
+		</Fragment>
+	);
 };
 
 export default Welcome;
