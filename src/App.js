9df4b4112278dadb8ea274b4475a7384ea8f7b83
mauriciogc <mauriciogc@gmail.com>
2020-04-15 16:26:08 -0500

    aphrodite with react-with-styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import React from 'react';
-import Component from './components/Component'
+import React from "react";
+import WithStylesContext from "react-with-styles/lib/WithStylesContext";
+import AphroditeInterface from "react-with-styles-interface-aphrodite";
 
-const App = () => <div className="App"><Component /></div>;
+import Component from "./components/Component";
+
+const theme = {
+	colors: {
+		primary: "#4A430B",
+		primaryVariant: "#3C3A2D",
+		background: "#F7DF1E",
+		secondary: "#F9EA6F",
+		secondaryVariant: "#FFFFFF"
+	}
+};
+
+const App = () => {
+	return (
+		<WithStylesContext.Provider
+			value={{
+				stylesInterface: AphroditeInterface,
+				stylesTheme: theme
+			}}
+		>
+			<div className="App">
+				<Component />
+			</div>
+		</WithStylesContext.Provider>
+	);
+};
 export default App;
--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { StyleSheet, css } from "aphrodite/no-important";
+import { withStyles } from "react-with-styles";
 
-const styles = StyleSheet.create({
+const styles = ({ colors }) => ({
 	wrapper: {
 		padding: "40px",
 		textAlign: "left",
-		backgroundColor: "#f7df1e",
-		color: "#3c3a2d"
+		backgroundColor: colors.background,
+		color: colors.primaryVariant
 	},
 	h1: {
 		fontSize: "30px",
@@ -19,40 +19,38 @@ const styles = StyleSheet.create({
 	button: {
 		fontSize: "14px",
 		fontWeight: "400",
-		backgroundColor: "#4a430b",
+		backgroundColor: colors.primary,
 		border: "none",
 		cursor: "pointer",
-		color: "#ffffff",
+		color: colors.secondaryVariant,
 		padding: "10px 20px",
 		borderRadius: "4px",
-		":hover": {
-			backgroundColor: "#342f08"
+		"&:hover": {
+			backgroundColor: colors.primaryVariant
 		}
 	},
 	a: {
 		backgroundColor: "transparent",
-		color: "#3c3a2d",
-		":hover": {
-			backgroundColor: "transparent"
-		}
+		color: colors.primaryVariant,
+		"&:hover": {}
 	}
 });
 
-const Component = props => {
+const Component = ({ css, styles }) => {
 	return (
-		<div className={css(styles.wrapper)}>
-			<h1 className={css(styles.h1)}>Example component CSS</h1>
-			<p className={css(styles.p)}>
+		<div {...css(styles.wrapper)}>
+			<h1 {...css(styles.h1)}>Example component CSS</h1>
+			<p {...css(styles.p)}>
 				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus,
 				temporibus labore voluptatibus aspernatur illo ad id sit nulla, a atque
 				doloribus itaque ab? Soluta, exercitationem illum provident quod iure
 				numquam?
 			</p>
-			<button className={css(styles.button)}>I'm a button</button>
+			<button {...css(styles.button)}>I'm a button</button>
 			<a
 				href="/#"
 				onClick={e => e.preventDefault()}
-				className={css(styles.button, styles.a)}
+				{...css(styles.button, styles.a)}
 			>
 				I'm a link
 			</a>
@@ -60,4 +58,4 @@ const Component = props => {
 	);
 };
 
-export default Component;
+export default withStyles(styles)(Component);
