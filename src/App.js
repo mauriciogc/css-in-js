f008a2c87e4b0ad31507807ccecd6f29f2bde253
mauriciogc <mauriciogc@gmail.com>
2020-04-16 16:33:24 -0500

    compose estyles

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Welcome from "./components/Welcome";
 const App = () => (
 	<div className="App">
 		<Welcome />
-		<Welcome name="Mauricio" />
 	</div>
 );
 export default App;
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
-	h1: {
-		fontSize: "30px",
-		textAlign: "center",
-		color: props => (props.primary ? "#e95678" : "#25b0bc")
+	button: {
+		fontSize: "14px",
+		fontWeight: "400",
+		backgroundColor: "#4a430b",
+		border: "none",
+		cursor: "pointer",
+		color: "#ffffff",
+		padding: "10px 20px",
+		borderRadius: "4px"
+	},
+	link: {
+		composes: "$button",
+		color: "#4a430b",
+		backgroundColor: "transparent"
 	}
 });
 
-const Welcome = ({ name = "Stranger" }) => {
-	const isStranger = name === "Stranger"; //(A)
-	const classes = useStyles({ primary: isStranger });
-
-	return <h1 className={classes.h1}>Hello {name}</h1>;
+const Welcome = () => {
+	const classes = useStyles();
+	return (
+		<Fragment>
+			<button className={classes.button}>Click me!!</button>
+			<a className={classes.link} href="/#" onClick={e => e.preventDefault()}>
+				Give me click too!!
+			</a>
+		</Fragment>
+	);
 };
 export default Welcome;
